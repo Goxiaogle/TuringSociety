81597e8c6f2f3c57e01bbced281d12cf03c5c211
林钟一六 <goxiaogle@gmail.com>
2022-06-22 16:57:52 +0800

    最初版本 - 无任何作用仅作测试

---
+++ src/main/java/com/goxiaoge/turingsociety/TuringSocietyApplication.java
@@ -0,0 +1,13 @@
+package com.goxiaoge.turingsociety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuringSocietyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuringSocietyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goxiaoge/turingsociety/TuringSocietyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goxiaoge.turingsociety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuringSocietyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
